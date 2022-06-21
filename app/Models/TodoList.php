b88c27e6e592766da34eb9f27868752a59554933
Denis <den.boltivets@gmail.com>
2022-06-21 21:03:29 +0300

    make TodoList model

---
+++ app/Models/TodoList.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TodoList extends Model
+{
+    protected $fillable = [
+        'title',
+        'is_completed',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(TodoListItem::class, 'todo_list_id');
+    }
+}
