13f23e6e354d2222bfb8d409743f06142ac64dd7
Denis <den.boltivets@gmail.com>
2022-06-22 18:18:07 +0300

    create TodoListItemService

---
+++ app/Services/TodoListItemService.php
@@ -0,0 +1,102 @@
+<?php
+
+use App\Exceptions\TodolistAccessRestrictedException;
+use App\Models\TodoList;
+use App\Models\TodoListItem;
+use Illuminate\Database\Eloquent\Collection;
+
+class TodoListItemService
+{
+    private TodoListService $todoListService;
+
+    /**
+     * @param TodoListService $todoListService
+     */
+    public function __construct(TodoListService $todoListService)
+    {
+        $this->todoListService = $todoListService;
+    }
+
+    /**
+     * @param int $todoListId
+     * @return Collection
+     * @throws TodolistAccessRestrictedException
+     */
+    public function getItems(int $todoListId): Collection
+    {
+        $todoList = $this->todoListService->getTodoList($todoListId);
+        $this->todoListService->checkAccess($todoList);
+
+        return $todoList->items;
+    }
+
+    /**
+     * @param int $todoListId
+     * @param string $title
+     * @param string|null $description
+     * @return TodoListItem
+     * @throws TodolistAccessRestrictedException
+     */
+    public function createItem(int $todoListId, string $title, string $description = null): TodoListItem
+    {
+        $todoList = $this->todoListService->getTodoList($todoListId);
+        $this->todoListService->checkAccess($todoList);
+        $item = new TodoListItem();
+        $item->todo_list_id = $todoList->id;
+        $item->title = $title;
+        $item->description = $description;
+
+        $item->save();
+        $item->refresh();
+
+        return $item;
+    }
+
+    /**
+     * @param TodoListItem $todoListItem
+     * @param string $title
+     * @param string|null $description
+     * @param bool $is_completed
+     * @return TodoListItem
+     * @throws TodolistAccessRestrictedException
+     */
+    public function updateItem(
+        TodoListItem $todoListItem,
+        string            $title,
+        string            $description = null,
+        bool              $is_completed = false
+    ): TodoListItem {
+        $this->todoListService->checkAccess($todoListItem->todoList);
+        $todoListItem->title = $title;
+        $todoListItem->description = $description;
+        $todoListItem->is_completed = $is_completed;
+        $todoListItem->save();
+
+        $this->actualiseListCompletion($todoListItem->todoList);
+
+        return $todoListItem;
+    }
+
+    /**
+     * @param TodoListItem $todoListItem
+     * @return bool
+     * @throws TodolistAccessRestrictedException
+     */
+    public function deleteItem(TodoListItem $todoListItem): bool
+    {
+        $this->todoListService->checkAccess($todoListItem->todoList);
+        return $todoListItem->delete();
+    }
+
+    private function actualiseListCompletion(TodoList $todoList): void
+    {
+        foreach ($todoList->items as $item) {
+            if (!$item->is_completed) {
+                $this->todoListService->setIsCompleted($todoList, false);
+                return;
+            }
+        }
+
+        $this->todoListService->setIsCompleted($todoList, true);
+    }
+}
