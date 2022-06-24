caaa48555393880b719ee6b256edf8347cb5d37b
Denis <den.boltivets@gmail.com>
2022-06-24 11:34:52 +0300

    fix request

--- app/Http/Requests/TodoListRequest.php
+++ app/Http/Requests/TodoListRequest.php
@@ -4,24 +4,18 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+/**
+ * @property-read string $title
+ */
 class TodoListRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
 
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'title' => 'required|string|min:3',
